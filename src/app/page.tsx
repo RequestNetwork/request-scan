29aa0a1e8ace5eb1a93ab6c36ac2ce12aa8b470d
rodrigopavezi <rodrigopavezi@gmail.com>
2024-08-22 13:17:22 -0300

    feat: change background color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import { RecentArea } from '@/components/recent-area';
 export default function Home() {
   return (
     <div className="grid">
-      <div className="col-start-1 row-start-1 bg-primary md:h-96"></div>
+      <div className="col-start-1 row-start-1 bg-emerald-900 md:h-96"></div>
       <div className="col-start-1 row-start-1">
         <div className="flex flex-1 flex-col md:gap-8 md:p-8">
           <SearchArea />
--- src/components/search-area.tsx
+++ src/components/search-area.tsx
@@ -7,7 +7,9 @@ export function SearchArea() {
   return (
     <div className="flex items-center justify-around p-2">
       <div className="flex flex-col items-start justify-start gap-3">
-        <h1 className="text-2xl font-bold">The Request Network Explorer</h1>
+        <h1 className="text-2xl font-bold text-white">
+          The Request Network Explorer
+        </h1>
         <Search />
       </div>
       <Image src="data.svg" alt="Data" width="300" height="300" />
