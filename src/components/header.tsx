d5d69b6407b752ff94fa6a0601ff2a80595d55ef
rodrigopavezi <rodrigopavezi@gmail.com>
2024-08-28 11:21:39 -0300

    feat: increase padding in the header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -12,7 +12,7 @@ export default function Header() {
 
   return (
     <div className="border-b">
-      <div className="flex h-16 items-center px-4">
+      <div className="flex h-16 items-center px-32">
         <Logo />
         <div className="ml-auto flex items-center space-x-4 px-4">
           {pathname !== '/' && <Search />}
