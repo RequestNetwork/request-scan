86e2dced12030bd474bb786a6f1614f395db8331
rodrigopavezi <rodrigopavezi@gmail.com>
2024-09-07 03:02:33 -0300

    feat: more detail and export pdf

--- src/app/request/[id]/page.tsx
+++ src/app/request/[id]/page.tsx
@@ -7,7 +7,6 @@ import {
   Card,
   CardHeader,
   CardTitle,
-  CardDescription,
   CardContent,
   CardFooter,
 } from '@/components/ui/card';
@@ -15,16 +14,24 @@ import { Skeleton } from '@/components/ui/skeleton';
 import { fetchRequest } from '@/lib/queries/channel';
 import { fetchRequestPayments } from '@/lib/queries/request-payments';
 import {
-  calculatePaymentReference,
+  calculateLongPaymentReference,
+  calculateShortPaymentReference,
   commonQueryOptions,
   formatTimestamp,
   getAmountWithCurrencySymbol,
+  getBalance,
+  getContentDataFromCreateTransaction,
+  getTransactionCreateParameters,
+  renderAddress,
 } from '@/lib/utils';
+import { ActorInfo } from '@requestnetwork/data-format';
 import { useQuery } from '@tanstack/react-query';
-import { Copy } from 'lucide-react';
+import { Copy, File } from 'lucide-react';
 import Link from 'next/link';
 import { redirect } from 'next/navigation';
 import TimeAgo from 'timeago-react';
+import { JsonEditor } from 'json-edit-react';
+import { useExportPDF } from '@/lib/hooks/use-export-pdf';
 
 interface RequestPageProps {
   params: {
@@ -32,15 +39,83 @@ interface RequestPageProps {
   };
 }
 
+const ActorInfoSection = ({ actorInfo }: { actorInfo?: ActorInfo }) => {
+  if (
+    !actorInfo ||
+    Object.keys(actorInfo).every(
+      (k) => !Object.keys((actorInfo as any)[k]).length,
+    )
+  ) {
+    return 'N/A';
+  }
+
+  return (
+    <div className="grid gap-3 bg-muted/50 rounded-md p-2">
+      <ul className="grid gap-3">
+        {actorInfo?.businessName && (
+          <li className="flex items-center justify-start gap-2">
+            <span className="text-muted-foreground">Business Name:</span>
+            <span>{actorInfo?.businessName}</span>
+          </li>
+        )}
+        {actorInfo?.firstName && (
+          <li className="flex items-center justify-start gap-2">
+            <span className="text-muted-foreground">First Name:</span>
+            <span>{actorInfo?.firstName}</span>
+          </li>
+        )}
+        {actorInfo?.lastName && (
+          <li className="flex items-center justify-start gap-2">
+            <span className="text-muted-foreground">Last Name:</span>
+            <span>{actorInfo?.lastName}</span>
+          </li>
+        )}
+        {actorInfo?.email && (
+          <li className="flex items-center justify-start gap-2">
+            <span className="text-muted-foreground">Email:</span>
+            <span>{actorInfo?.email}</span>
+          </li>
+        )}
+        {actorInfo?.phone && (
+          <li className="flex items-center justify-start gap-2">
+            <span className="text-muted-foreground">Phone:</span>
+            <span>{actorInfo?.phone}</span>
+          </li>
+        )}
+        {actorInfo?.address && Object.keys(actorInfo?.address).length > 0 && (
+          <li className="flex items-center justify-start gap-2">
+            <span className="text-muted-foreground">Address:</span>
+            <span>{renderAddress(actorInfo)}</span>
+          </li>
+        )}
+        {actorInfo?.taxRegistration && (
+          <li className="flex items-center justify-start gap-2">
+            <span className="text-muted-foreground">Tax Registration:</span>
+            <span>{actorInfo?.taxRegistration}</span>
+          </li>
+        )}
+        {actorInfo?.miscellaneous! && (
+          <li className="flex items-center justify-start gap-2">
+            <span className="text-muted-foreground">Miscellaneous:</span>
+            <span>{JSON.stringify(actorInfo?.miscellaneous!)}</span>
+          </li>
+        )}
+      </ul>
+    </div>
+  );
+};
+
 export default function RequestPage({ params: { id } }: RequestPageProps) {
+  const { exportPDF } = useExportPDF();
+
   const { data: request, isLoading: isLoadingRequest } = useQuery({
     queryKey: ['request', id],
     queryFn: () => fetchRequest({ id }),
     ...commonQueryOptions,
   });
 
-  const reference = request
-    ? calculatePaymentReference(
+  const shortPaymentReference = request
+    ? calculateShortPaymentReference(
         id,
         request?.transactions[0].dataObject.data.parameters.extensionsData[0]
           .parameters.salt || '',
@@ -49,10 +124,14 @@ export default function RequestPage({ params: { id } }: RequestPageProps) {
       )
     : '';
 
+  const longPaymentReference = shortPaymentReference
+    ? calculateLongPaymentReference(shortPaymentReference)
+    : '';
+
   const { data: requestPayments, isLoading: isLoadingRequestPayments } =
     useQuery({
-      queryKey: ['request-payments', reference],
-      queryFn: () => fetchRequestPayments({ reference }),
+      queryKey: ['request-payments', longPaymentReference],
+      queryFn: () => fetchRequestPayments({ reference: longPaymentReference }),
       ...commonQueryOptions,
     });
 
@@ -68,13 +147,43 @@ export default function RequestPage({ params: { id } }: RequestPageProps) {
   const lastTransaction =
     request?.transactions[request.transactions.length - 1];
 
+  const balance = getBalance(requestPayments);
+
+  const createParameters = getTransactionCreateParameters(firstTransaction);
+  const contentData = getContentDataFromCreateTransaction(createParameters);
+  const buyerData = contentData?.buyerInfo;
+  const sellerData = contentData?.sellerInfo;
+
+  const status =
+    balance >= BigInt(createParameters.expectedAmount)
+      ? 'Paid'
+      : lastTransaction?.dataObject?.data?.name;
+
+  const modifiedTimestamp =
+    requestPayments &&
+    requestPayments[requestPayments?.length - 1]?.timestamp >
+      lastTransaction?.blockTimestamp
+      ? requestPayments[requestPayments?.length - 1]?.timestamp
+      : lastTransaction?.blockTimestamp;
+
+  const handleExportPDF = () => {
+    exportPDF({
+      ...contentData,
+      currency: createParameters.currency,
+      currencyInfo: createParameters.currency,
+      payer: createParameters.payer,
+      payee: createParameters.payee,
+      expectedAmount: createParameters.expectedAmount,
+    });
+  };
+
   return (
     <div className="h-full pt-24 pb-32">
       <div className="w-[95%] bg-white border rounded-lg self-center md:w-full">
         <Card className="overflow-hidden" x-chunk="dashboard-05-chunk-4">
           <CardHeader className="flex flex-row items-start bg-muted/50">
-            <div className="grid gap-0.5">
-              <CardTitle className="group flex items-center gap-2 text-lg break-all">
+            <div className="grid gap-0.5 w-full">
+              <CardTitle className="group flex items-center gap-2 text-lg break-all w-full justify-between">
                 Request {id}
                 <Button
                   size="icon"
@@ -85,78 +194,127 @@ export default function RequestPage({ params: { id } }: RequestPageProps) {
                   }}
                 >
                   <Copy className="h-3 w-3" />
-                  <span className="sr-only">Copy Order ID</span>
+                </Button>
+                <Button
+                  size="sm"
+                  className="h-8 gap-1"
+                  onClick={handleExportPDF}
+                >
+                  <File className="h-3.5 w-3.5" />
+                  <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
+                    Export PDF
+                  </span>
                 </Button>
               </CardTitle>
-              {firstTransaction?.blockTimestamp && (
-                <CardDescription>
-                  Timestamp:{' '}
-                  <TimeAgo
-                    datetime={firstTransaction.blockTimestamp * 1000}
-                    locale="en_short"
-                  />{' '}
-                  ({formatTimestamp(firstTransaction.blockTimestamp)})
-                </CardDescription>
-              )}
             </div>
           </CardHeader>
           <CardContent className="p-6 text-sm">
-            <div className="grid gap-3">
-              <ul className="grid gap-3">
-                <li className="flex items-center justify-start gap-2">
-                  <span className="text-muted-foreground">Status:</span>
-                  <span>{lastTransaction?.dataObject?.data?.name}</span>
-                </li>
-                <li className="flex items-center justify-start gap-2">
-                  <span className="text-muted-foreground">Payee:</span>
-                  <div className="font-medium text-emerald-700 break-all">
-                    <Link
-                      href={`/address/${firstTransaction?.dataObject?.data?.parameters?.payee?.value}`}
-                    >
-                      {
-                        firstTransaction?.dataObject?.data?.parameters?.payee
-                          ?.value
-                      }{' '}
-                    </Link>
-                  </div>
-                </li>
-                <li className="flex items-center justify-start gap-2">
-                  <span className="text-muted-foreground">Payer:</span>
-                  <div className="font-medium text-emerald-700 break-all">
-                    <Link
-                      href={`/address/${firstTransaction?.dataObject?.data?.parameters?.payer?.value}`}
-                    >
-                      {
-                        firstTransaction?.dataObject?.data?.parameters?.payer
-                          ?.value
-                      }{' '}
-                    </Link>
-                  </div>
-                </li>
-                <li className="flex items-center justify-start gap-2">
-                  <span className="text-muted-foreground">
-                    Expected Amount:
-                  </span>
-                  <span>
+            <table className="border-separate border-spacing-y-3">
+              <tbody>
+                <tr>
+                  <td className="text-muted-foreground">Status:</td>
+                  <td className="pl-16">{status}</td>
+                </tr>
+                <tr>
+                  <td className="text-muted-foreground">Payee:</td>
+                  <td className="pl-16">
+                    <div className="font-medium text-emerald-700 break-all">
+                      <Link href={`/address/${createParameters.payee?.value}`}>
+                        {createParameters.payee?.value}
+                      </Link>
+                    </div>
+                  </td>
+                </tr>
+                <tr>
+                  <td className="text-muted-foreground">Payee Details:</td>
+                  <td className="pl-16">
+                    <ActorInfoSection actorInfo={sellerData} />
+                  </td>
+                </tr>
+                <tr>
+                  <td className="text-muted-foreground">Payer:</td>
+                  <td className="pl-16">
+                    <div className="font-medium text-emerald-700 break-all">
+                      <Link href={`/address/${createParameters.payer?.value}`}>
+                        {createParameters.payer?.value}
+                      </Link>
+                    </div>
+                  </td>
+                </tr>
+                <tr>
+                  <td className="text-muted-foreground">Payer Details:</td>
+                  <td className="pl-16">
+                    <ActorInfoSection actorInfo={buyerData} />
+                  </td>
+                </tr>
+                <tr>
+                  <td className="text-muted-foreground">Expected Amount:</td>
+                  <td className="pl-16">
                     {getAmountWithCurrencySymbol(
-                      request?.transactions[0].dataObject.data.parameters
-                        .expectedAmount,
-                      request?.transactions[0].dataObject.data.parameters
-                        .currency.value,
+                      BigInt(createParameters.expectedAmount),
+                      createParameters.currency.value,
                     )}
-                  </span>
-                </li>
-                <li className="flex items-center justify-start gap-2">
-                  <span className="text-muted-foreground">Blockchain:</span>
-                  <span>
-                    {
-                      firstTransaction?.dataObject?.data?.parameters?.currency
-                        ?.network
-                    }
-                  </span>
-                </li>
-              </ul>
-            </div>
+                  </td>
+                </tr>
+                <tr>
+                  <td className="text-muted-foreground">Balance:</td>
+                  <td className="pl-16">
+                    {getAmountWithCurrencySymbol(
+                      BigInt(balance),
+                      createParameters.currency.value,
+                    )}
+                  </td>
+                </tr>
+                <tr>
+                  <td className="text-muted-foreground">Created:</td>
+                  <td className="pl-16">
+                    <TimeAgo
+                      datetime={firstTransaction.blockTimestamp * 1000}
+                      locale="en_short"
+                    />{' '}
+                    ({formatTimestamp(firstTransaction.blockTimestamp)})
+                  </td>
+                </tr>
+                <tr>
+                  <td className="text-muted-foreground">Modified:</td>
+                  <td className="pl-16">
+                    <TimeAgo
+                      datetime={modifiedTimestamp * 1000}
+                      locale="en_short"
+                    />{' '}
+                    ({formatTimestamp(modifiedTimestamp)})
+                  </td>
+                </tr>
+                <tr>
+                  <td className="text-muted-foreground">Payment Reference:</td>
+                  <td className="pl-16">{shortPaymentReference}</td>
+                </tr>
+                <tr>
+                  <td className="text-muted-foreground">Blockchain:</td>
+                  <td className="pl-16">
+                    {createParameters.currency?.network}
+                  </td>
+                </tr>
+                <tr>
+                  <td className="text-muted-foreground">Raw Content Data:</td>
+                  <td className="pl-16">
+                    <div className="bg-muted/50 rounded-md p-2">
+                      <JsonEditor
+                        data={contentData}
+                        restrictEdit={true}
+                        restrictDelete={true}
+                        restrictAdd={true}
+                        restrictDrag={true}
+                        restrictTypeSelection={true}
+                        theme="githubLight"
+                        collapse={true}
+                        rootFontSize={14}
+                      />
+                    </div>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
           </CardContent>
           <CardFooter className="flex flex-row items-center border-t bg-muted/50 px-6 py-3">
             <div className="grid grid-cols-1 gap-4 w-full">
